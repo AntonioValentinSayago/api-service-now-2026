3f9cbf8ff0d0154fa2bf31da4b32e83b60d8514c
AntonioValentinSayago <cesar_anto30@hotmail.com>
2026-02-19 23:45:31 -0600

    feat(api): bootstrap Node.js + TypeScript REST API with Prisma ORM and MySQL integration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Node + TS, test');
+import express  from "express";
+import cors from 'cors';
+
+export const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (_req, res) => {
+  res.json({ ok: true });
+});
