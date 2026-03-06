721d9f77a6ee8ded51bd4313afe997ec0f251cd9
AntonioValentinSayago <cesar_anto30@hotmail.com>
2026-03-05 22:05:05 -0600

    refactor(api): move Prisma queries to service layer for better separation of concerns

--- src/controllers/apps.controller.ts
+++ src/controllers/apps.controller.ts
@@ -1,7 +1,22 @@
-import type { Request, Response } from "express";
+import type { Request, Response, NextFunction } from "express";
 import { prisma } from "../config/prisma";
 
-export async function listApps(_req: Request, res: Response) {
-  const apps = await prisma.apps.findMany({ orderBy: { id: "desc" } });
-  return res.json(apps);
+export async function listApps(_req: Request, res: Response, next: NextFunction) {
+  try {
+    const apps = await prisma.apps.findMany({ orderBy: { id: "asc" } });
+    return res.status(200).json({
+     success: true,
+     code: 200, 
+     message: apps.length ? "Apps retrieved successfully" : "Data Not Found ",
+     data: apps 
+    });
+  } catch (error) {
+    next(error)
+    console.log('Server Not Found');
+    return res.status(500).json({
+      success: false,
+      code: 500,
+      message: 'Servidor no Disponible'
+    })
+  }
 }
